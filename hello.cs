c44dc083c2724ce51f97da1ada7d00dc8599f997
chagu-maro <ogashouta@gmail.com>
2024-07-07 17:26:20 +0900

    first commit

---
+++ hello.cs
@@ -0,0 +1,11 @@
+using System;
+namespace myapp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# World");
+        }
+    }
+}
---
+++ hello.vb
@@ -0,0 +1,5 @@
+Module Module1
+    Sub Main()
+        Console.WriteLine("Hello VB World")
+    End Sub
+End Module
